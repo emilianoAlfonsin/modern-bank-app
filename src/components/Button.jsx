88a046c2ff62f48771c42cb18cc0195d409be744
EmilianoAlfonsin <emiliano.a.83@gmail.com>
2023-01-21 09:28:36 -0300

    Card Deal section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Button = ({ styles, text }) => (
-  <button type='button' className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-black text-[18px] outlined-none ${styles}`} >
+  <button type='button' className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-black text-[18px] outlined-none rounded-[10px] ${styles}`} >
     {text}
   </button>
 )
